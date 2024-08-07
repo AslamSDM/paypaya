90f03937422143e12cf70717aae2ed180760c072
AslamSDM <aslamprpd@gmail.com>
2024-08-07 11:52:42 +0530

    update

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import { Twilio, twiml } from 'twilio';
 import bodyParser from 'body-parser';
 import { config } from 'dotenv';
 
@@ -7,16 +6,15 @@ const app = express();
 const port = 3000;
 
 // Twilio credentials
-const accountSid = process.env.TWILIO_SSID;
-
-const authToken = process.env.TWILIO_TOKEN;
-
-const client = new Twilio(accountSid, authToken);
 
 // Middleware to parse incoming requests
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 app.post('/sms', (req, res) => {
+  console.log('Received SMS:', req.body);
+
   const { Body, From, To, MessageSid } = req.body;
 
   // Convert SMS data to JSON
@@ -30,12 +28,7 @@ app.post('/sms', (req, res) => {
 
   console.log('Received SMS:', JSON.stringify(smsJson, null, 2));
 
-  // Send a response back to Twilio
-  const twimlResponse = new twiml.MessagingResponse();
-  twimlResponse.message('SMS received and processed.');
 
-  res.writeHead(200, { 'Content-Type': 'text/xml' });
-  res.end(twimlResponse.toString());
 });
 
 app.listen(port, () => {

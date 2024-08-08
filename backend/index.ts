8730eac4d85d0bda70500b2e8825636cb7eb3769
AslamSDM <aslamprpd@gmail.com>
2024-08-08 15:44:40 +0530

    update

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {  ethers } from 'ethers';
+import { ethers } from 'ethers';
 import Dispatcherabi from '../contracts/artifacts/contracts/Dispatcher.sol/Dispatcher.json'
+import USDCabi from '../contracts/artifacts/contracts/USDC.sol/USDC.json
 import { supabase } from './db/supabase';
 import jwt from 'jsonwebtoken';
 
@@ -9,9 +10,10 @@ const app = express();
 const port = 3000;
 
 const provider = new ethers.JsonRpcProvider('https://rpc-mumbai.maticvigil.com');
-const wallet = new ethers.Wallet(process.env.PRIVATE_KEY??"", provider);
+const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "", provider);
 const signer = wallet.connect(provider);
-const dispatcher = new ethers.Contract(process.env.DISPATCHER_CONTRACT??"", Dispatcherabi.abi, signer);
+const dispatcher = new ethers.Contract(process.env.DISPATCHER_CONTRACT ?? "", Dispatcherabi.abi, signer);
+const USDC = new ethers.Contract(process.env.USDC_CONTRACT ?? "", USDCabi, signer);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -19,39 +21,42 @@ app.get('/', (req, res) => {
 app.post('/sms', async (req, res) => {
   console.log('Received SMS:', req.body);
 
-  const { sender ,content,msgId,rcvd} = req.body;
+  const { sender, content, msgId, rcvd } = req.body;
   const contentjson = JSON.parse(content);
-  console.log('Received SMS:', contentjson); 
+  console.log('Received SMS:', contentjson);
 
   switch (contentjson.type) {
     case "send":
       const to = contentjson.to;
-      const amount = ethers.parseUnits(contentjson.amount,6)
-      const fromaddress_data  = await supabase.from('users').select('address').eq('phone',sender).single()
-      if(fromaddress_data.error){
+      const amount = ethers.parseUnits(contentjson.amount, 6)
+      const fromaddress_data = await supabase.from('users').select('address').eq('phone', sender).single()
+      const toaddress_data = await supabase.from('users').select('address').eq('phone', to).single()
+      if (fromaddress_data.error || toaddress_data.error) {
         return res.send('User not found');
       }
+      const approval = await USDC.allowance(fromaddress_data.data.address, process.env.DISPATCHER_CONTRACT ?? "");
+      if (approval.lt(amount)) return res.send('Insufficient balance');
       const from = fromaddress_data.data.address;
-
-      const tx =dispatcher.send(from,to, amount).then((tx) => {
+      const toaddress = toaddress_data.data.address;
+      const tx = dispatcher.send(from, toaddress, amount).then((tx) => {
         console.log('Transaction:', tx);
         return res.send('Transaction sent');
       });
-      await supabase.from('transactions').insert([{from:from,to:to,amount:amount.toString(),status:"pending",msgId:msgId,rcvd:rcvd}])
+      await supabase.from('transactions').insert([{ from: from, to: to, amount: amount.toString(), status: "pending" }])
 
       break;
     case "verify":
       const msgId = contentjson.msgId;
-      if(!contentjson.token){
+      if (!contentjson.token) {
         return res.send('Token not found');
       }
-      const token = jwt.verify(contentjson?.token, process.env.JWT_SECRET??"");
+      const token = jwt.verify(contentjson?.token, process.env.JWT_SECRET ?? "");
       const { error } = await supabase.from('users').update({ address: token.address }).eq('phone', sender);
       if (error) {
         return res.send('Error updating user');
       }
       return res.send('User updated successfully');
-      }
+  }
 });
 
 app.listen(port, () => {

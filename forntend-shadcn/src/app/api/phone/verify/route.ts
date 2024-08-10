60a5f6ae8655a62e814cd6f783b639e842a06667
AslamSDM <aslamprpd@gmail.com>
2024-08-11 00:02:34 +0530

    added phone verification api

---
+++ forntend-shadcn/src/app/api/phone/verify/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+
+export async function POST(req: NextRequest) {
+  const { address } = await req.json();
+
+  const token = jwt.sign({ data: address }, process.env.JWT_SECRET ?? "", {
+    expiresIn: "1h",
+  });
+
+  return NextResponse.json({
+    message: JSON.stringify({
+      type: "verify",
+      token: token,
+    }),
+  });
+}
